bd5a097644f87c345be4436b050d075aee26c37d
ShenanigansBunul <sergiiusalceanu@gmail.com>
2020-04-17 20:10:38 +0300

    t4?????????

--- T4/T4.py
+++ T4/T4.py
@@ -23,6 +23,10 @@ def read_list(fname):
     return list(map(lambda x: float(x), f_arr)), f_size
 
 
+a, a_size = read_rare("a_example.txt")
+b, b_size = read_list("b_example.txt")
+
+
 def empty_rare_matrix(n):
     result = []
     for _ in range(n):
@@ -44,74 +48,70 @@ def rare_matrix(data, n):
                 j[0] += params[0]
         if not found:
             result[int(params[1])].append([params[0], int(params[2])])
-    return result
+
+    result2 = []
+    for i in range(n):
+        result2.append(dict())
+    for i in range(n):
+        for j in result[i]:
+            result2[i][str(j[1])] = j[0]
+    return result2
 
 
 def diag_check(m, n):
     for i in range(n):
-        non_null = False
-        for j in m[i]:
-            if j[1] == i:
-                if j[0] != 0:
-                    non_null = True
-        if not non_null:
+        if str(i) not in m[i]:
+            print(m[i])
             return "Matricea nu are diagonala nenula"
     return "Matricea are diagonala nenula"
 
 
-def gauss_seidel(m, x, b, n):  # sistem m, solutie x, termeni liberi b, dimensiune n
+def g_sum1(row, x, i):
+    sum1 = 0
+    for j in range(i-1):
+        factor = 0
+        if str(j) in row:
+            factor = row[str(j)]
+        sum1 += factor * x[j]
+    return sum1
+
+
+def g_sum2(row, x, n,  i):
+    sum2 = 0
+    for j in range(i, n):
+        factor = 0
+        if str(j) in row:
+            factor = row[str(j)]
+        sum2 += factor * x[j]
+    return sum2
+
+
+def gauss_seidel(m, b, n, x):  # sistem m, termeni liberi b, dimensiune n, sir anterior x
+    new_x = zeros(n)
     for i in range(n):
-        d = b[i]
-        for j in range(n):
-            if j != i:
-                m_el = 0
-                for k in m[i]:
-                    if k[1] == j:
-                        m_el = k[0]
-                        break
-                d = d - m_el * x[i]
-        m_el = 0
-        for k in m[i]:
-            if k[1] == i:
-                m_el = k[0]
-                break
-        x[i] = d / m_el
-    return x
+        row = m[i]
+        new_x[i] = (b[i] - g_sum1(row, new_x, i) - g_sum2(row, x, n, i)) / row[str(i)]
+    return new_x
 
 
 def initial_x(n):
     result = []
     for i in range(n):
-        result.append(i+1)
+        result.append(i + 1)
     return result
 
 
-a_1, a_1_size = read_rare("a_1.txt")
-a_2, a_2_size = read_rare("a_2.txt")
-a_3, a_3_size = read_rare("a_3.txt")
-a_4, a_4_size = read_rare("a_4.txt")
-a_5, a_5_size = read_rare("a_5.txt")
-b_1, b_1_size = read_list("b_1.txt")
-b_2, b_2_size = read_list("b_2.txt")
-b_3, b_3_size = read_list("b_3.txt")
-b_4, b_4_size = read_list("b_4.txt")
-b_5, b_5_size = read_list("b_5.txt")
+def zeros(n):
+    result = []
+    for i in range(n):
+        result.append(0)
+    return result
 
-a_1_m = rare_matrix(a_1, a_1_size)
-a_2_m = rare_matrix(a_2, a_2_size)
-a_3_m = rare_matrix(a_3, a_3_size)
-a_4_m = rare_matrix(a_4, a_4_size)
-a_5_m = rare_matrix(a_5, a_5_size)
 
-print(diag_check(a_1_m, a_1_size))
-print(diag_check(a_2_m, a_2_size))
-print(diag_check(a_3_m, a_3_size))
-print(diag_check(a_4_m, a_4_size))
-print(diag_check(a_5_m, a_5_size))
+a_matrix = rare_matrix(a, a_size)
+print(diag_check(a_matrix, a_size))
 
-x = initial_x(a_1_size)
-
+x = initial_x(a_size)
 for i in range(25):
-    x = gauss_seidel(a_1_m, x, b_1, a_1_size)
-
-print(x)
+    x = gauss_seidel(a_matrix, b, a_size, x)
+    print(x)
